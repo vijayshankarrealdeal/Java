1d85d093cb008d0d0dc7c7cc1c50c77a19c9f097
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-09 22:09:48 +0530

    Password Reset has some error

--- JavaProject/app/src/main/java/com/example/myapplication/ForgetPassword.java
+++ JavaProject/app/src/main/java/com/example/myapplication/ForgetPassword.java
@@ -22,7 +22,6 @@ public class ForgetPassword extends AppCompatActivity {
     Button submitEmailButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
         forgetEmail = findViewById(R.id.forgetPass);
