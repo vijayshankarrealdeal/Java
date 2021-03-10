51863e4e16e41eba8e05caf0283ff0faa1835eb7
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-10 14:18:56 +0530

    finalCommit

--- Admin-Scripts/adminScripts.py
+++ Admin-Scripts/adminScripts.py
@@ -1,5 +1,37 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials,firestore
+from firebase_admin import storage
+cred = credentials.Certificate("./adminKey.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'women-e598c.appspot.com'
+})
+#Database Methods
+db = firestore.client()
 
-cred = credentials.Certificate("women-e598c-firebase-adminsdk-2nvya-385a16cd62.json")
-firebase_admin.initialize_app(cred)
+discrip = ""
+
+
+title = ""
+cloudStorageLink = ""
+name = ""
+source = ""
+sourceLink = ""
+discription = discrip
+viewsOnVideo = 19,21,167
+socialHandle = ""
+webpage = ""
+
+if(len(title)!=0 and len(cloudStorageLink)!=0):
+    videsoWrite =  db.collection("adminContent").document("Videos").collection("data").document().set({
+        "title":title,
+        "name":name,
+        "source":source,
+        "sourceLink":sourceLink,
+        "discription":discription,
+        "viewsOnVideo":viewsOnVideo,
+        "socialHandle":socialHandle,
+        "webpage":webpage,
+        "cloudStorageLink":cloudStorageLink
+    }) 
+else:
+    print("Error")
