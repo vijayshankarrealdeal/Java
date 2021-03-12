5646276980bbd3ed3899d66eca2b1f1d6b8d0d97
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-12 18:57:23 +0530

    Day 12

--- JavaProject/app/src/main/java/com/example/myapplication/Dashboard.java
+++ JavaProject/app/src/main/java/com/example/myapplication/Dashboard.java
@@ -8,13 +8,40 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Dashboard extends AppCompatActivity {
-
+    FirebaseFirestore cloudStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         BottomNavigationView bottomNavigationView  =findViewById(R.id.bottom_navigation);
