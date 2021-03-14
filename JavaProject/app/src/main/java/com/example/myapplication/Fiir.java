4c211c39af336e5aadac5e6c63169d7fef3571db
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-03-14 20:27:03 +0530

    Day 14

--- JavaProject/app/src/main/java/com/example/myapplication/Fiir.java
+++ JavaProject/app/src/main/java/com/example/myapplication/Fiir.java
@@ -2,13 +2,82 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Fiir extends AppCompatActivity {
+    Button navigate;
+    EditText nameOfPerson;
+    EditText emailOfPerson;
+    EditText crimeOfPerson;
+    EditText culpitOfPerson;
+    EditText culpritDetails;
+    EditText phoneNumber;
+    EditText yourAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fiir);
+        navigate = findViewById(R.id.nextPageTO);
+
+        nameOfPerson = findViewById(R.id.nameOfPerson);
+        emailOfPerson = findViewById(R.id.emailOfPerson);
+        crimeOfPerson = findViewById(R.id.crimeOfPerson);
+        culpitOfPerson = findViewById(R.id.culpitOfPerson);
+        culpritDetails = findViewById(R.id.culpritDetails);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        yourAddress = findViewById(R.id.yourAddress);
+
+
+        navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameOfPe = nameOfPerson.getText().toString().trim();
+                String emailOfPe = emailOfPerson.getText().toString().trim();
+                String crimeOfPe = crimeOfPerson.getText().toString().trim();
+                String culpitOfPe = culpitOfPerson.getText().toString().trim();
+                String culpritDe = culpritDetails.getText().toString().trim();
+                String phoneNu = phoneNumber.getText().toString().trim();
+                String address =  yourAddress.getText().toString().trim();
+                if (TextUtils.isEmpty(nameOfPe))
+                {
+                    nameOfPerson.setError("Name is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(crimeOfPe))
+                {
+                    crimeOfPerson.setError("Add the Crime is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(phoneNu) && phoneNu.length()!=10)
+                {
+                    phoneNumber.setError("Add your phone number");
+                    return;
+                }
+                if (TextUtils.isEmpty(address))
+                {
+                    phoneNumber.setError("Add your Address");
+                    return;
+                }
+
+
+                Intent i = new Intent(Fiir.this,FirPage2.class);
+                i.putExtra("nameOfPerson",nameOfPe);
+                i.putExtra("emailOfPerson",emailOfPe);
+                i.putExtra("crimeOfPerson",crimeOfPe);
+                i.putExtra("culpitOfPerson",culpitOfPe);
+                i.putExtra("culpritDetails",culpritDe);
+                i.putExtra("phoneNumber",phoneNu);
+                i.putExtra("phoneNumber",address);
+
+                startActivity(i);
+            }
+        });
     }
 }
--- JavaProject/app/src/main/java/com/example/myapplication/FirPage2.java
+++ JavaProject/app/src/main/java/com/example/myapplication/FirPage2.java
@@ -11,30 +11,115 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 
 public class FirPage2 extends AppCompatActivity {
 
     private static final int CAMERA_PERM_CODE =111 ;
     ImageView selectedImage;
     Button cameraBtn;
+    TextView addh;
+    Button finalSubmit;
+
+    String nameOfPerson;
+    String emailOfPerson;
+    String crimeOfPerson;
+    String culpitOfPerson;
+    String culpritDetails;
+    String phoneNumber;
+    String address;
+    Bitmap image;
+    FirebaseAuth auth;
+    FirebaseStorage Fstorage;
+    StorageReference sref;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fir_page2);
+
+        nameOfPerson  = getIntent().getStringExtra("nameOfPerson");
+        emailOfPerson  = getIntent().getStringExtra("emailOfPerson");
+        crimeOfPerson  = getIntent().getStringExtra("crimeOfPerson");
+        culpitOfPerson  = getIntent().getStringExtra("culpitOfPerson");
+        culpritDetails  = getIntent().getStringExtra("culpritDetails");
+        phoneNumber = getIntent().getStringExtra("phoneNumber");
+        address = getIntent().getStringExtra("address");
+
+
         selectedImage = findViewById(R.id.imageIdOfPerson);
         cameraBtn = findViewById(R.id.openCamera);
-
+        addh = findViewById(R.id.AddharCardNumber);
+        finalSubmit = findViewById(R.id.SubmitButtonAllThings);
+        Fstorage = FirebaseStorage.getInstance();
+        auth  = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        sref = Fstorage.getReference();
+        HashMap<String,String> hashMap = new HashMap<>();
+        StorageReference imagesRef = sref.child("images");
+        Bitmap bitmap = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        FirebaseUser user = auth.getCurrentUser();
+        UploadTask uploadTask = sref.child("hello").child(user.getUid()).putBytes(data);
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 askCameraPermission();
+            }
+        });
+        finalSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        String url = taskSnapshot.getStorage().getDownloadUrl().toString();
+                        hashMap.put("url",url);
+                        firebaseFirestore.collection("Users").document(user.getUid())
+                                .collection("ComplainsFir").document().set(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                            System.out.println("DOne");
+                            }
+                        });
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+
             }
         });
     }
@@ -71,7 +156,7 @@ public class FirPage2 extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == 102)
         {
-            Bitmap image =(Bitmap) data.getExtras().get("data");
+            image =(Bitmap) data.getExtras().get("data");
             selectedImage.setImageBitmap(image);
         }
     }
