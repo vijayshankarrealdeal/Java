0e088bb881d26450af6386d289063e45b3b3922c
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-04-02 21:19:07 +0530

    Update About.java

--- JavaProject/app/src/main/java/com/example/myapplication/About.java
+++ JavaProject/app/src/main/java/com/example/myapplication/About.java
@@ -1,62 +1,80 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.annotation.NonNull;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.app.ActivityCompat;
+    import androidx.core.content.ContextCompat;
+    import android.Manifest;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.pm.PackageManager;
+    import android.location.Address;
+    import android.location.Geocoder;
+    import android.location.Location;
+    import android.location.LocationListener;
+    import android.location.LocationManager;
+    import android.nfc.Tag;
+    import android.os.Bundle;
+    import android.util.Log;
+    import android.view.MenuItem;
+    import android.view.View;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import android.content.Intent;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
+    import com.google.android.gms.tasks.OnSuccessListener;
+    import com.google.android.material.bottomnavigation.BottomNavigationView;
+    import com.google.firebase.auth.FirebaseAuth;
+    import com.google.firebase.auth.FirebaseUser;
+    import com.google.firebase.firestore.DocumentSnapshot;
+    import com.google.firebase.firestore.FirebaseFirestore;
+    import com.squareup.picasso.Picasso;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+    import java.io.IOException;
+    import java.util.List;
+    import java.util.Locale;
 
-import org.w3c.dom.Document;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class About extends AppCompatActivity {
-
-    FirebaseAuth auth;
-    ImageView imageView;
-    TextView textView;
-    LocationListener locationListener;
+public class About extends AppCompatActivity implements LocationListener {
     LocationManager locationManager;
-    String mediaUrL;
+    LocationListener locationListener;
+    ImageView graphX;
+    FirebaseAuth auth;
+    TextView editLocation;
     private  FirebaseFirestore firebaseFirestore;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=
+                    PackageManager.PERMISSION_GRANTED){
+                // Check
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,5000,100,locationListener);
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        graphX = findViewById(R.id.graphXX);
         firebaseFirestore = FirebaseFirestore.getInstance();
-        imageView = findViewById(R.id.showGraphImage);
-        textView = findViewById(R.id.jkbdsjfdvshf);
+        editLocation = findViewById(R.id.editLocation);
+
+        //Image
         firebaseFirestore.collection("crimeData").document("graph").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot snapshot) {
-                mediaUrL = snapshot.getString("imageUrl");
-                textView.setText(mediaUrL);
-                System.out.println(snapshot.getString("imageUrl"));
+                Picasso
+                        .get()
+                        .load(snapshot.getString("imageUrl"))
+                        .into(graphX);
             }
         });
-        Glide.with(this).load(mediaUrL).into(imageView);
-        System.out.println(mediaUrL);
-        ///Location
-
 
 
 
@@ -84,5 +102,52 @@ public class About extends AppCompatActivity {
         });
     }
 
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                editLocation.setText("");
 
-}
+                Toast.makeText(
+                        getBaseContext(),
+                        "Location changed: Lat: " + location.getLatitude() + " Lng: "
+                                + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                String longitude = "Longitude: " + location.getLongitude();
+                String latitude = "Latitude: " + location.getLatitude();
+
+                /*------- To get city name from coordinates -------- */
+                String cityName = null;
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<Address> addresses;
+                try {
+                    addresses = gcd.getFromLocation(location.getLatitude(),
+                            location.getLongitude(), 1);
+                    if (addresses.size() > 0) {
+                        System.out.println(addresses.get(0).getLocality());
+                        cityName = addresses.get(0).getLocality();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                        + cityName;
+                editLocation.setText(s);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+
+
+        }
